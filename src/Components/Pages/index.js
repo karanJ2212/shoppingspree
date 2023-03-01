7491fff82f6c6c36f9d3286cdd96cf3aa61948fb
karan jain <karanjain2212@gmail.com>
2023-03-01 16:15:02 +0530

    Folder Structure Initiated

---
+++ src/Components/Pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./HomePage/Homepage";
+import Category from "./CategoryPage/CategoryPage";
+import Cart from "./CartPage/CartPage";
+
+export { Home, Category, Cart };
---
+++ src/Components/SingleProduct/SingleProduct.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleProduct = () => {
+  return <div>single SingleProduct</div>;
+};
+
+export default SingleProduct;
