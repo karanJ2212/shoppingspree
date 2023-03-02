41156fb6ee4940d4816bd63e9123d4e5454276cd
karan jain <karanjain2212@gmail.com>
2023-03-02 13:57:52 +0530

    navbar scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import "./App.css";
+import "./App.scss";
 
-import { BrowserRouter, route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, Category, Cart } from "./Components/Pages/index";
 
-import Navbar from "./Components/Navbar";
-import Loader from "./Components/Loader";
-import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Loader from "./Components/Loader/Loader";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/Cart" element={<Cart></Cart>}></Route>
+          <Route path="/category/:id" element={<Category></Category>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,56 @@
 import React from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="container">
+          <div className="navbar-top flex flex-between">
+            <Link to="/" className="navbar-brand">
+              <span className="text-regal-blue">Shopping</span>
+              <span className="text-gold">Spree</span>
+            </Link>
+            <form className="navbar-search flex">
+              <input type="text" placeholder="search here..." />
+              <button type="submit" className="navbar-search-btn">
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
+            <div className="navbar-btns">
+              <Link to="/cart" className="add-to-cart-btn flex">
+                <span className="btn-ico">
+                  <i className="fas fa-shopping-cart"></i>
+                </span>
+                <div className="btn-txt fw-5">
+                  cart <span className="cart-count-value">0</span>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-bottom bg-regal-blue">
+          <div className="container flex flex-between">
+            <ul className="nav-links flex">
+              <button type="button" className="navbar-hide-btn text-white">
+                <i className="fas fa-times"></i>
+              </button>
+              <li>
+                <Link to="/" className="nav-link text-white">
+                  Demos
+                </Link>
+              </li>
+            </ul>
+            <button type="button" className="navbar-show-btn text-gold">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
