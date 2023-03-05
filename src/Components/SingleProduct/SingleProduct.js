06b05b9b7f5bf2c878a1006ffb45480f4e06c796
karan jain <karanjain2212@gmail.com>
2023-03-05 16:02:13 +0530

    Add to cart button

--- src/Components/SingleProduct/SingleProduct.js
+++ src/Components/SingleProduct/SingleProduct.js
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SingleProduct.scss";
 import { setIsModalVisible } from "../../store/ModalSlice";
 import { formatPrice } from "../../utils/helpers";
 import { useDispatch, useSelector } from "react-redux";
+import { add2Cart } from "../../store/CartSlice";
+import { useNavigate } from "react-router-dom";
 
 const SingleProduct = () => {
   const dispatch = useDispatch();
-
-  const addToCartHandler = (product) => {
-    console.log(product);
-  };
+  const navigate = useNavigate();
+  const [qty, setQty] = useState(1);
 
   const { data: product } = useSelector((state) => state.modal);
+
+  const increaseQty = () => {
+    setQty((prevQty) => {
+      let newQty = prevQty + 1;
+      return newQty;
+    });
+  };
+  const decreaseQty = () => {
+    setQty((prevQty) => {
+      let newQty = prevQty - 1;
+      if (newQty < 1) {
+        newQty = 1;
+      }
+      return newQty;
+    });
+  };
+
+  const addToCartHandler = (product) => {
+    let totalPrice = qty * product.price;
+    const tempProduct = {
+      ...product,
+      quantity: qty,
+      totalPrice,
+    };
+    dispatch(add2Cart(tempProduct));
+    dispatch(setIsModalVisible(false));
+    navigate("/cart");
+  };
+
+  const modalOverlayHandler = (e) => {
+    if (e.target.classList.contains("overlay-bg")) {
+      dispatch(setIsModalVisible(false));
+    }
+  };
+
   return (
-    <div className="overlay-bg">
-      <div className="product-detail-modal bg-white">
+    <div className="overlay-bg" onClick={modalOverlayHandler}>
+      <div className="product-details-modal bg-white">
         <button
           type="button"
           className="modal-close-btn flex flex-center fs-14"
@@ -23,31 +58,39 @@ const SingleProduct = () => {
           <i className="fas fa-times"></i>
         </button>
         <div className="details-content grid">
-          <div className="details-left">
+          {/* details left */}
+          <div className="details-right">
             <div className="details-img">
               <img src={product.images[0]} alt={product.title} />
             </div>
           </div>
-          {/* details left */}
-          <div className="details-right">
+          {/* detials right */}
+          <div className="details-left">
             <div className="details-info">
               <h3 className="title text-regal-blue fs-22 fw-5">
                 {product.title}
               </h3>
-              <p className="description text-pine-green"></p>
-              <div className="price fw-7 fs-23">
-                Price:{formatPrice(product.price)}
+              <p className="description text-pine-green">
+                {product.description}
+              </p>
+              <div className="price fw-7 fs-24">
+                Price: {formatPrice(product.price)}
               </div>
               <div className="qty flex">
-                <span className="text-light-blue qty-text">Qty:</span>
+                <span className="text-light-blue qty-text">Qty: </span>
                 <div className="qty-change flex">
-                  <button type="button" className="qty-dec fs-14">
+                  <button
+                    type="button"
+                    className="qty-dec fs-14"
+                    onClick={() => decreaseQty()}
+                  >
                     <i className="fas fa-minus text-light-blue"></i>
                   </button>
-                  <span className="qty-value flex flex-center"></span>
+                  <span className="qty-value flex flex-center">{qty}</span>
                   <button
                     type="button"
                     className="qty-inc fs-14 text-light-blue"
+                    onClick={() => increaseQty()}
                   >
                     <i className="fas fa-plus"></i>
                   </button>{" "}
@@ -56,7 +99,7 @@ const SingleProduct = () => {
               <button
                 type="button"
                 className="btn-primary add-to-cart-btn"
-                onClick={addToCartHandler(product)}
+                onClick={() => addToCartHandler(product)}
               >
                 <span className="btn-icon">
                   <i className="fas fa-cart-shopping"></i>
